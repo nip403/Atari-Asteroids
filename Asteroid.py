359282441121e8a9948b4a01572cd69d8d50dc51
nip403 <41174986+nip403@users.noreply.github.com>
2018-08-26 16:28:00 +0800

    Update Asteroid.py

--- Asteroid.py
+++ Asteroid.py
@@ -2,18 +2,25 @@ import pygame
 import math
 import random
 
-class Asteroid:
-    maxstage = 5
-    
-    def __init__(self,surf,stage=None,pos=[0,0],vector=[0,0],speed=1.5):
-        self.stage = self.maxstage if stage is None else stage
+class Base_stats:
+    linear_divide = True
+    divide_amount = 2
+    base_stage = 8
+    _speed = 1.5
+
+class Asteroid(Base_stats):    
+    def __init__(self,surf,stage=None,pos=[0,0],vector=[0,0],speed=None):
+        self.stage = self.base_stage if stage is None else stage
         self.size = self.stage*2 + 1
         self.surf = surf
 
-        self.speed = speed
-        self.radius = self.size * 15
+        self.speed = self._speed if speed is None else speed
+        self.radius = self.size * 10
 
-        if self.stage >= self.maxstage:
+        self.rotation = 0
+        self.directionR = random.randint(0,1)
+
+        if self.stage == self.base_stage:
             if random.randint(0,1):
                 self.pos = [random.randint(0,self.surf.get_width()),0 if random.randint(0,1) else self.surf.get_height()]
             else:
@@ -33,19 +40,17 @@ class Asteroid:
         if self.stage <= 1:
             return []
         else:
-            a = Asteroid(self.surf,self.stage-1,[i+random.randint(-10,10) for i in self.pos],self.rotate(self.vector,-80),self.speed+0.2)
-            b = Asteroid(self.surf,self.stage-1,[i+random.randint(-10,10) for i in self.pos],self.rotate(self.vector,80),self.speed+0.2)
-
-            return [Asteroid(self.surf,self.stage-1,[i+random.randint(-10,10) for i in self.pos],self.rotate(self.vector,360/(abs(self.stage-self.maxstage)+2) * j),self.speed+0.2) for j in range(abs(self.stage-self.maxstage)+2)]
+            return [Asteroid(self.surf,self.stage-1,[i+random.randint(-10,10) for i in self.pos],self.rotate(self.vector,360/(abs(self.stage-self.base_stage)+2 if not self.linear_divide else self.divide_amount) * j+90),self.speed+0.2) for j in range(abs(self.stage-self.base_stage)+2 if not self.linear_divide else self.divide_amount)]
 
     def rotate(self,vector,deg):
         return [vector[0]*math.cos(math.radians(deg)) - vector[1]*math.sin(math.radians(deg)),vector[0]*math.sin(math.radians(deg)) + vector[1]*math.cos(math.radians(deg))]
 
     def draw(self):
-        pygame.draw.polygon(self.surf,(255,255,255),[[self.pos[0]+self.radius*math.cos(math.radians(i)),self.pos[1]+self.radius*math.sin(math.radians(i))] for i in range(0,360,int(360/self.size))],1)
+        pygame.draw.polygon(self.surf,(255,255,255),[[self.pos[0]+self.radius*math.cos(math.radians(i+self.rotation)),self.pos[1]+self.radius*math.sin(math.radians(i+self.rotation))] for i in range(0,360,int(360/self.size))],1)
 
     def move(self):
         self.pos = [self.pos[i] + self.vector[i] for i in range(2)]
+        self.rotation += 1 if self.directionR else -1
 
         if self.pos[0] < -self.radius:
             self.pos[0] = self.surf.get_width()+self.radius
